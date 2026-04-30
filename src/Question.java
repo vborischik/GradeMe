1a3107a11265212affae7af4f2b00c3954ce2180
Vlad <mail@mail.com>
2026-04-30 18:09:28 -0500

    Initial commit

---
+++ src/Question.java
@@ -0,0 +1,15 @@
+public class Question {
+    private String question;
+    private String[] options;
+    private String answer;
+
+    public Question(String question, String[] options, String answer) {
+        this.question = question;
+        this.options = options;
+        this.answer = answer;
+    }
+
+    public String getQuestion() { return question; }
+    public String[] getOptions() { return options; }
+    public String getAnswer() { return answer; }
+}
